5d728e9786bec4bccd3f7351cf224e1b0c9c0d14
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-09-20 22:26:23 +0530

    added two subtests

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -7,6 +7,9 @@ const englishHelloPrefix = "Hello, "
 
 // Hello returns greeting.
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name
 }
 
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,16 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
+	// below are two subtests.
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
-func Benchmark(b *testing.B) {
-	for i := 1; i <= b.N; i++ {
-		Hello("Chris")
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
