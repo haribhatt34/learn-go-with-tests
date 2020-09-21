1c2c6e3c5a6ca83f6b5728df3eed8d55dfdc915b
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-09-21 10:46:15 +0530

    added language parameter in hello function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -6,13 +6,16 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 // Hello returns greeting.
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -18,17 +18,23 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	// below are two subtests.
+	// below are subtests.
 	// syntax (name, func)
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Chris", "Spanish")
+		want := "Hola, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 }
