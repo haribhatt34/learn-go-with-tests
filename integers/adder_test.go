d615f07c00038e724eae2afca57b21336939eabc
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-09-22 13:04:00 +0530

    final code of integer, added example and refactored

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	sum := Add(3, 5)
@@ -9,4 +12,22 @@ func TestSum(t *testing.T) {
 	if sum != expected {
 		t.Errorf("got %d want %d", sum, expected)
 	}
+}
+
+/*
+ * Examples are compiled as part of test suite.
+ * 1) Should start with Example, just like Test.
+ * 2) Use the same format as below like fmt.
+ * 3) To see example getting testing use
+ *		go test -v
+ * 4) Must use commented out //Output: value,
+ *    else it won't compile
+ *    It test output expected value with the fmt value.
+ * 5) godoc will contain our defined functions with examples,
+ *    which we defined below. (godoc -http=:6060)
+ */
+func ExampleAdd() {
+	sum := Add(1, 2)
+	fmt.Println(sum)
+	// Output: 3
 }
