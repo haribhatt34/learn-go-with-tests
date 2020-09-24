65e2f0eb34904bf43ca82cf784f762907f6deebf
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-09-24 19:27:50 +0530

    updated slices with sum

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,11 +2,29 @@ package arrays
 
 import "testing"
 
+// Test Coverage -
+// go test -cover.
+// if TDD is strictly followed, test coverage is near to 100%.
+
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
-	}
+
+	//array
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
+
+	//slice
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
 }
