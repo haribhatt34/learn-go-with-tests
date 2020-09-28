90964041e80801c088b8006a3bf5ee47f2aa18c9
Hari Shankar Bhatt <haribhatt34@gmail.com>
2020-09-28 14:36:47 +0530

    refactored structs

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,15 +2,26 @@ package structs
 
 import "math"
 
-//Rectangle defines the parameter of rectangle
+// Shape is an interface for rectangel and circle
+type Shape interface {
+	Area() float64
+}
+
+// Rectangle defines the parameter of rectangle
 type Rectangle struct {
 	length  float64
 	breadth float64
 }
 
-//Circle defines the parameter of rectangle
+// Circle defines the parameter of Circle
 type Circle struct {
 	radius float64
+}
+
+// Triangle defines the parameter of Triangle
+type Triangle struct {
+	base   float64
+	height float64
 }
 
 // Perimeter function returns perimeter of rectangle
@@ -26,4 +37,9 @@ func (r Rectangle) Area() float64 {
 // Area function returns area of circle
 func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
+}
+
+// Area function returns area of triangle
+func (t Triangle) Area() float64 {
+	return 0.5 * t.base * t.height
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,7 +1,6 @@
 package structs
 
 import (
-	"math"
 	"testing"
 )
 
@@ -20,23 +19,75 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
+	/* By using interface, we have decoupled the helper method from
+	 * the concrete types (rectange, circle), now we can pass any
+	 * type which is a shape and get the job done.
+	 * In an essence code is also refactored, we don't have to write
+	 * assert case for every comparison.
+	 * If an object is passed which is not shaped, it will safely fail.
+	 */
+
+	/* Change to table based test below
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{2.0, 3.0}
-		got := rectangle.Area()
 		want := 6.0
+		checkArea(t, rectangle, want)
 
-		if got != want {
-			t.Errorf("got %g want %.2f", got, want)
-		}
 	})
 
 	t.Run("circles", func(t *testing.T) {
-		circle := Circle{2.0}
-		got := circle.Area()
-		want := 4 * math.Pi
-
-		if got != want {
-			t.Errorf("got %g want %.2f", got, want)
-		}
+		circle := Circle{10.0}
+		want := 314.15926
+		checkArea(t, circle, want)
 	})
+	*/
+
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{2.0, 3.0}, hasArea: 6.0},
+		{name: "Circle", shape: Circle{10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
+
+/* Quote:
+ * In Test-Driven Deveopment by Example Kent Beck refactors some tests to
+ * a point and asserts:
+ * The test speaks to us more clearly, as if it were an assertion of truth,
+ * not a sequenc of operations.
+ */
+
+/* Notes:
+ * Declaring interface helps in defining functions that can be used by
+ * different types (parameteric polymorphism)
+ * Adding methods so we can add functionality to our data types and also
+ * we can implement interfaces.
+ * Table based test like above, make assertions clearer and easier to
+ * extend & maintain.
+ */
+
+/* Important:
+ * Interfaces are a great way to hide complexity.
+ * In our case test helper didn't need to know the exact shape it was
+ * asserting on, it only need to know the it area.
+ */
